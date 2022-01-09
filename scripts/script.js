29892e33983fffd9e9d1491f465e47246d3e8a24
servosdesign <servos.design0@gmail.com>
2022-01-08 21:49:32 -0600

    organizing html, updating project list, adding files

--- scripts/script.js
+++ scripts/script.js
@@ -3,11 +3,11 @@ function showDiv() {
  const text = document.getElementById('footer-click');
  const img = document.getElementById('footer-click2');
 
-if (text.style.visibility == "hidden" && img.style.visibility == "hidden"){
-  text.style.visibility = "visible";
-  img.style.visibility = "visible";
-} else {
-  text.style.visibility = "hidden";
-  img.style.visibility = "hidden"; 
-}
-}
+  if (text.style.visibility == "hidden" && img.style.visibility == "hidden"){
+    text.style.visibility = "visible";
+    img.style.visibility = "visible";
+  } else {
+    text.style.visibility = "hidden";
+    img.style.visibility = "hidden"; 
+  }
+};

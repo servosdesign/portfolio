5018862bf53c476964cd7aecf0700bc5c9089928
servosdesign <servos.design0@gmail.com>
2023-01-17 15:52:45 -0600

    projects page, config setup, img imports

---
+++ src/components/PortfolioDisplay.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <ProjectsContent />
+    <DesignsContent />
+  </div>
+</template>
+
+<script>
+import ProjectsContent from './ProjectsContent.vue'
+import DesignsContent from './DesignsContent.vue'
+
+export default {
+  components: {
+    ProjectsContent,
+    DesignsContent
+  },
+  data () {
+    return {}
+  }
+}
+</script>

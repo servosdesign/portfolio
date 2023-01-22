964ed21d90aeecb5b3ec0d2e564367cc65f0c9bd
servosdesign <servos.design0@gmail.com>
2023-01-22 10:48:58 -0600

    old portfolio

--- portfolio-old/scripts/script.js
+++ portfolio-old/scripts/script.js
@@ -2,14 +2,14 @@ function showDiv() {
   const text = document.getElementById('footer-click');
   const img = document.getElementById('footer-click2');
 
-if (window.innerWidth > 1000){
- 
-    if (text.style.visibility == "hidden" && img.style.visibility == "hidden"){
+  if (window.innerWidth > 1000) {
+
+    if (text.style.visibility == "hidden" && img.style.visibility == "hidden") {
       text.style.visibility = "visible";
       img.style.visibility = "visible";
     } else {
       text.style.visibility = "hidden";
-      img.style.visibility = "hidden"; 
+      img.style.visibility = "hidden";
     }
     if (text.style.marginLeft === "925px" && img.style.marginLeft === "1075px") {
       text.style.marginLeft = "0";
@@ -18,13 +18,13 @@ if (window.innerWidth > 1000){
       text.style.marginLeft = "925px";
       img.style.marginLeft = "1075px";
     }
-} else {
-    if (text.style.visibility == "hidden" && img.style.visibility == "hidden"){
+  } else {
+    if (text.style.visibility == "hidden" && img.style.visibility == "hidden") {
       text.style.visibility = "visible";
       img.style.visibility = "visible";
     } else {
       text.style.visibility = "hidden";
-      img.style.visibility = "hidden"; 
+      img.style.visibility = "hidden";
     }
   };
 };
